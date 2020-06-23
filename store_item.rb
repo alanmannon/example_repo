964d0f54c4864bcc7ece49ae4627fc7d392bfd37
Alan <alanmannon@gmail.com>
2020-06-23 18:36:45 -0500

    added firstdocument

--- store_item.rb
+++ store_item.rb
@@ -3,11 +3,11 @@
 # computer = { :price => 1500, :color => "black", aisle: 15 }
 
 class Produce
-  def initialize(name, price, color, aisle)
-    @name = name
-    @price = price
-    @color = color
-    @aisle = aisle
+  def initialize(input_hash = {})
+    @name = :name
+    @price = :price
+    @color = :color
+    @aisle = :aisle
   end
 
   def instock(currently_in_stock)
@@ -15,9 +15,9 @@ class Produce
   end
 end
 
-bread = Produce.new("bread", 3, "white", 2)
-gasoline = Produce.new("gasoline", 2, "orange", 3)
-computer = Produce.new("computer", 1500, "black", 15)
+bread = Produce.new(name: "bread", price: 3, color: "white", aisle: 2)
+gasoline = Produce.new(name: "gasoline", price: 2, color: "orange", aisle: 3)
+computer = Produce.new(name: "computer", price: 1500, color: "black", aisle: 15)
 
 p bread
 p gasoline
